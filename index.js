a4ce3880adb40f0653465e4b6d19dd6e380edb54
Timo Weiß <info@timo-weiss.com>
2017-02-16 13:19:09 +0100

    use tabs

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 module.exports = {
-    extends: [
-        'eslint-config-airbnb-base',
-    ].map(require.resolve),
-    rules: {
-        indent: ['error', 'tab'],
-        'no-tabs': 0,
-        strict: 2,
-        'class-methods-use-this': 0,
-    },
-    compilerOptions: {
-        target: 'ES6',
-        module: 'commonjs',
-    },
+	extends: [
+		'eslint-config-airbnb-base',
+	].map(require.resolve),
+	rules: {
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+		strict: 2,
+		'class-methods-use-this': 0,
+	},
+	compilerOptions: {
+		target: 'ES6',
+		module: 'commonjs',
+	},
 };
--- react.js
+++ react.js
@@ -1,36 +1,36 @@
 module.exports = {
-    extends: [
-        'eslint-config-airbnb',
-    ].map(require.resolve),
-    parser: 'babel-eslint',
-    rules: {
-        indent: ['error', 'tab'],
-        'no-tabs': 0,
-        'react/jsx-indent-props': [2, 'tab'],
-        'react/jsx-indent': [2, 'tab'],
-        'no-undef': 0,
-        'react/jsx-no-undef': 1,
-        'import/no-require': 1,
-        'import/no-named-default': 1,
-        'import/no-named-as-default': 1,
-        'import/no-named-as-default-member': 1,
-        'react/prefer-stateless-function': 1,
-        'no-underscore-dangle': 1,
-        'import/first': 1,
-        'jsx-a11y/no-static-element-interactions': 1,
-        'react/jsx-filename-extension': [
-            1,
-            {
-                extensions: [
-                    '.js',
-                    '.jsx',
-                ],
-            },
-        ],
-    },
-    parserOptions: {
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-        },
-    },
+	extends: [
+		'eslint-config-airbnb',
+	].map(require.resolve),
+	parser: 'babel-eslint',
+	rules: {
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+		'react/jsx-indent-props': [2, 'tab'],
+		'react/jsx-indent': [2, 'tab'],
+		'no-undef': 0,
+		'react/jsx-no-undef': 1,
+		'import/no-require': 1,
+		'import/no-named-default': 1,
+		'import/no-named-as-default': 1,
+		'import/no-named-as-default-member': 1,
+		'react/prefer-stateless-function': 1,
+		'no-underscore-dangle': 1,
+		'import/first': 1,
+		'jsx-a11y/no-static-element-interactions': 1,
+		'react/jsx-filename-extension': [
+			1,
+			{
+				extensions: [
+					'.js',
+					'.jsx',
+				],
+			},
+		],
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			experimentalObjectRestSpread: true,
+		},
+	},
 };
