d78c64ada02dd8b6908eef48edc56192988f5273
Timo Weiß <info@timo-weiss.com>
2017-02-21 15:25:30 +0100

    Add initial eslint configs for node and react projects (#1)
    * update orga in refs
    
    * add airbnb configs as dep
    
    * rm
    
    * init rules
    
    * update readme
    
    * use default for react/prop-types
    
    * major version bump
    
    * set rule to use tabs for everything
    
    * set rule to use tabs
    
    * use tabs
    
    * remove deprecated rule
    
    * use default for import/first
    
    * use default for react/prefer-stateless-function
    
    * update node eslint config based on dl-server-core

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+	extends: [
+		'eslint-config-airbnb-base',
+	].map(require.resolve),
+	rules: {
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+		'class-methods-use-this': 0,
+		'comma-dangle': ['error', 'never'],
+		'import/no-extraneous-dependencies': ['error', { 'devDependencies': true }],
+		'max-len': ['error', 120],
+		'no-underscore-dangle': 0,
+		strict: 0
+	},
+	compilerOptions: {
+		target: 'ES6',
+		module: 'commonjs',
+	},
+};
