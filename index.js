37753c0e85cb89cc679e44a64f3e4e7069216317
Michael Raue <michael.raue@tensegrity.de>
2017-09-26 17:28:03 +0200

    exclude no-named-default

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = {
 		'import/no-extraneous-dependencies': ['error', { 'devDependencies': true }],
 		'max-len': ['error', 120],
 		'no-underscore-dangle': 0,
+		'import/no-named-default':  0,
 		strict: 0,
 		'no-param-reassign': 0,
 		'no-prototype-builtins': 0
